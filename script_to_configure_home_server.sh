6d74bd33c3c8f3e24db51e61ccddd01e4c1a2de1
Lukasz <kolodzinski.lukasz@gmail.com>
2018-04-26 10:17:04 +0200

    added driver check in script to configure home server

--- script_to_configure_home_server.sh
+++ script_to_configure_home_server.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 driver = "USER INPUT"
 read -p "Type driver name - for example: i40en-2.0.6: " driver
-find /vmfs/volumes/datastore1/ -name "$driver*"
+drivercheck=`find /vmfs/volumes/datastore1/ -name "$driver*"`
+if [ -n "$drivercheck" ]; then
+	echo "Found: $drivercheck"
+else
+	echo "Driver not found"
+fi
+
