d4bd4604e1ab273700246eed5236e75af25caebb
Lukasz <kolodzinski.lukasz@gmail.com>
2018-04-27 11:20:26 +0200

    Added: disabling server firewall and check firewall current status

--- script_to_configure_home_server.sh
+++ script_to_configure_home_server.sh
@@ -4,8 +4,12 @@ read -p "Type driver name - for example: i40en-2.0.6: " driver
 drivercheck=`find /vmfs/volumes/datastore1/ -name "$driver*"`
 if [ -n "$drivercheck" ]; then
 	echo "1.Found: $drivercheck"
-	echo "2.Unloading firewall"
+	echo "2.Disabling firewall..."
+	esxcli network firewall set -e false
+	echo "3.Unloading firewall..."
 	esxcli network firewall unload
+	echo "4.Firewall current status:"
+	esxcli network firewall get
 else
 	echo "Driver not found"
 fi
