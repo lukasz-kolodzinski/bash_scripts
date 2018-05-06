78f6820218fbea497c77301b7f2488e3e5e55ede
Łukasz Kołodziński <luk.kolodzinski@gmail.com>
2018-05-06 20:59:15 +0200

    Added: list devices connected with PCI bus

--- hardware_basics.sh
+++ hardware_basics.sh
@@ -3,6 +3,9 @@ cd /home/$USER
 mkdir Device_info
 cd Device_info
 touch hardware_basics
-echo "Kernel version" >> hardware_basics
+echo "Kernel version:" >> hardware_basics
 uname -r >> hardware_basics
+echo "CPU info:" >> hardware_basics
 lscpu >> hardware_basics
+echo "Devices connected with PCI bus:" >> hardware_basics
+lspci >> hardware_basics
