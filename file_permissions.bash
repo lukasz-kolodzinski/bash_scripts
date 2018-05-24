392569edfd452bc25b83ee8e65b0fc2ef75e3dea
Lukasz <kolodzinski.lukasz@gmail.com>
2018-05-24 14:12:19 +0200

    Added: chmod for all user types

--- file_permissions.bash
+++ file_permissions.bash
@@ -5,4 +5,6 @@ echo "Display octal value of permissions"
 stat -c %a test_file1
 echo "Display simbolic value of permissions"
 stat -c %A test_file1
+chmod u=rwx,g=rwx,o=wx test_file1
+ls -l
 rm test_file1
