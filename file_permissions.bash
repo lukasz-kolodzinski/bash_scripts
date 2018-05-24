423c84a6a000c35ec3f1c9a102e060994405e38c
Lukasz <kolodzinski.lukasz@gmail.com>
2018-05-24 13:22:03 +0200

    Added: show symbolic value of permissions

--- file_permissions.bash
+++ file_permissions.bash
@@ -1,5 +1,8 @@
 #/bin/bash -xv
 cd /home/$USER
 touch test_file1
+echo "Display octal value of permissions"
 stat -c %a test_file1
+echo "Display simbolic value of permissions"
+stat -c %A test_file1
 rm test_file1
