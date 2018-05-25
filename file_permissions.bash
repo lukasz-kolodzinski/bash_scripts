236e555cd1fec4a89586041854f032d889ae4c25
Lukasz <kolodzinski.lukasz@gmail.com>
2018-05-25 12:10:52 +0200

    Added: remove execute permission for u/g/o

--- file_permissions.bash
+++ file_permissions.bash
@@ -5,6 +5,10 @@ echo "Display octal value of permissions"
 stat -c %a test_file1
 echo "Display simbolic value of permissions"
 stat -c %A test_file1
+echo "Set user/group/other permissions with simbolic values"
 chmod u=rwx,g=rwx,o=wx test_file1
 ls -l
+echo "Retreat u/g/o execute  permission "
+chmod u-x,g-x,o-x test_file1
+stat -c %A test_file1
 rm test_file1
